8ea9c0920d6cc211f989f938afbb56c37e7736d7
lomoye <834033206@qq.com>
2019-03-06 16:42:51 +0800

    ok

--- app/js/home/Money.vue
+++ app/js/home/Money.vue
@@ -18,7 +18,7 @@ import Panel from '../core/Panel';
 export default {
    data() {
        return {
-           items: [{
+            items: [{
                 title: "定期理财",
                 sub: "理财首选",
                 rate: "5.60%",
@@ -55,9 +55,66 @@ export default {
     @include flex(row);
     justify-content: space-around;
     box-sizing: border-box;
+    &:after {
+      content: " ";
+      display: block;
+      width: 100%;
+      height: 0px;
+      box-sizing: border-box;
+      border-bottom: 1px solid #ddd;
+      position: relative;
+      top: -208px;
+    }
     .item {
       position: relative;
+      box-sizing: border-box;
       width: 50%;
+      &:after {
+        content: " ";
+        width: 1px;
+        height: 136px;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        margin-top: -68px;
+        border-right: 1px solid #eee;
+      }
+      &:nth-child(2n) {
+        &:after {
+          display: none;
+        }
+      }
+      padding: 34px 16px;
+      dt {
+        font-size: 30px;
+        line-height: 42px;
+        color: #333;
+        span {
+          font-size: 22px;
+          color: #ff5155;
+          border: 1px solid #ff5155;
+          padding: 0 8px;
+          vertical-align: 1px;
+          margin-top: 1px;
+        }
+      }
+      dd {
+        &:nth-child(2) {
+          font-weight: 700;
+          font-size: 44px;
+          height: 58px;
+          line-height: 58px;
+          color: #ff5155;
+          @include ellipsis;
+        }
+        &:nth-child(3) {
+          font-size: 24px;
+          height: 34px;
+          line-height: 34px;
+          color: #999;
+          @include ellipsis;
+        }
+      }
     }
   }
 }
